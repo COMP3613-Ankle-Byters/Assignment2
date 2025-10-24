6738b2ea61c89964589db9715c4b49445763d666
Nat-B997 <nb2216515@gmail.com>
2025-10-24 17:08:04 -0400

    Fixed Errors

--- App/views/auth.py
+++ App/views/auth.py
@@ -1,27 +1,59 @@
-from flask_jwt_extended import create_access_token
-from App.models import db, Student, Staff
-from .student import create_student
-from .staff import create_staff
-from functools import wraps
+from flask import Blueprint, render_template, jsonify, request, flash, redirect
+from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
+from App.controllers.auth import login
 
-def login(first_name, last_name, password):
-    user = Student.query.filter_by(first_name=first_name).first()
-    if not user:
-        user = Staff.query.filter_by(last_name=last_name).first()
+auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
 
-    if not user or not user.check_password(password):
-        return None
+# Page/HTML routes
+@auth_views.route('/login', methods=['POST'])
+def login_action():
+    data = request.form
+    token = login(data['username'], data['password'])
+    response = redirect(request.referrer)
+    if not token:
+        flash('Bad username or password', 'error')
+        return response, 401
+    flash('Login successful', 'success')
+    set_access_cookies(response, token)
+    return response
 
-    token = create_access_token(
-    identity=str(user.id),
-    additional_claims={"type": "student" if isinstance(user, Student) else "staff"}
+@auth_views.route('/logout', methods=['GET'])
+def logout_action():
+    response = redirect(request.referrer)
+    flash("Logged out!", 'info')
+    unset_jwt_cookies(response)
+    return response
+
+@auth_views.route('/identify', methods=['GET'])
+@jwt_required()
+def identify_page():
+    return render_template(
+        'message.html',
+        title="Identify",
+        message=f"You are logged in as {current_user.id} - {current_user.username}"
     )
-    return token
 
+# API routes
+@auth_views.route('/api/login', methods=['POST'])
+def login_api():
+    data = request.json
+    token = login(data['username'], data['password'])
+    if not token:
+        return jsonify({'message': 'Bad username or password'}), 401
+    response = jsonify(access_token=token)
+    set_access_cookies(response, token)
+    return response
 
-def initialize():
-    db.drop_all()
-    db.create_all()
-    staff = create_staff("Alice", "Smith", "staffpass")
-    student = create_student("Bob", "Brown", "studentpass")
-    print(f"Created sample users: {staff.first_name}, {student.first_name}")
+@auth_views.route('/api/logout', methods=['GET'])
+def logout_api():
+    response = jsonify(message="Logged out!")
+    unset_jwt_cookies(response)
+    return response
+
+@auth_views.route('/api/identify', methods=['GET'])
+@jwt_required()
+def identify_user_api():
+    return jsonify({
+        'username': current_user.username,
+        'id': current_user.id
+    })
